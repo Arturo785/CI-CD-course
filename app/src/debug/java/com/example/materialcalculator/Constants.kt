1e34aecea37a90075cc1a9a02d2eb8616a384216
Jorge Rodriguez <vn53fry@m-c02fn2pnmd6m.local.homeoffice.wal-mart.com>
2022-10-17 17:16:11 -0500

    addition of build variant

---
+++ app/src/debug/java/com/example/materialcalculator/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.materialcalculator
+
+
+// this one is only used in the debug apk when we generated because is in the debug main folder
+// the other variants access the other main source folder
+// the files we can access depends on the build variant we are using
+object Constants {
+    const val BASE_URL = "TEST_URL"
+}
